523171440e77a0e14e15772028c98c37ce610e2e
igor-rgomes <74742354+igor-rgomes@users.noreply.github.com>
2025-04-20 12:55:10 -0300

    Update search.rs

--- src/modules/search.rs
+++ src/modules/search.rs
@@ -15,7 +15,7 @@ pub fn search(query: &str, index: &HashMap<String, Vec<usize>>) -> Vec<usize> {
     }
 
     let mut results: Vec<(usize, usize)> = scores.into_iter().collect();
-    results.sort_by(|a, b| b.1.cmp(&a.1)); // ordena por relevância
+    results.sort_by(|a, b| b.1.cmp(&a.1));
 
     results.into_iter().map(|(id, _)| id).collect()
 }
