42ce417ddcf818f2e7dca02d59f444fd41e29635
igor-rgomes <74742354+igor-rgomes@users.noreply.github.com>
2025-04-20 12:48:37 -0300

    Update search.rs

--- src/modules/search.rs
+++ src/modules/search.rs
@@ -1,19 +1,21 @@
 use std::collections::HashMap;
 
 pub fn search(query: &str, index: &HashMap<String, Vec<usize>>) -> Vec<usize> {
-    let mut result_counter: HashMap<usize, usize> = HashMap::new();
     let lower = query.to_lowercase();
     let tokens = lower.split_whitespace();
+
+    let mut scores: HashMap<usize, usize> = HashMap::new();
 
     for token in tokens {
         if let Some(ids) = index.get(token) {
             for id in ids {
-                *result_counter.entry(*id).or_insert(0) += 1;
+                *scores.entry(*id).or_insert(0) += 1;
             }
         }
     }
 
-    let mut results: Vec<(usize, usize)> = result_counter.into_iter().collect();
-    results.sort_by(|a, b| b.1.cmp(&a.1));
+    let mut results: Vec<(usize, usize)> = scores.into_iter().collect();
+    results.sort_by(|a, b| b.1.cmp(&a.1)); // ordena por relevância
+
     results.into_iter().map(|(id, _)| id).collect()
 }
